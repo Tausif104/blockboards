a78f103d6e8e32aa34c2ac93347109e6115cff62
tausif104 <tausifahmed49@gmail.com>
2023-06-27 00:13:16 +0600

    brand fixed

--- src/components/Brands/Brands.jsx
+++ src/components/Brands/Brands.jsx
@@ -1,10 +1,11 @@
 import Slider from 'react-slick'
+import { Col, Container, Row } from 'react-bootstrap'
+import './brands.css'
 import LogoOne from './images/1.png'
 import LogoTwo from './images/2.png'
 import LogoThree from './images/3.png'
 import LogoFour from './images/4.png'
 import LogoFive from './images/5.png'
-import { Col, Container, Row } from 'react-bootstrap'
 
 const Brands = () => {
   const settings = {
@@ -21,7 +22,7 @@ const Brands = () => {
         <Row>
           <Col>
             <div className='section-title'>
-              <h2>Trusted by the top Projects in web3</h2>
+              <h3>Trusted by the top Projects in web3</h3>
             </div>
             <Slider className='logo-slider' {...settings}>
               <div className='brand-item'>
